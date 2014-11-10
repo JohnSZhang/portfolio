a499e65e06979785b5aee881aecee5300c217665
John Zhang <john.shimin.zhang@gmail.com>
2014-11-10 16:12:42 -0500

    tyr without js css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,8 +5,5 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( jquery.min.js skel.min.js static.js.coffee init.js )
-Rails.application.config.assets.precompile += %w( skel.css font-awesome.min.css skel.scss _vars.scss _mixins.scss style-normal.scss style-normal.css style.css)
 Rails.application.config.assets.precompile += %w( header.jpg top-1600.svg top-1280.svg top-3200.svg bottom-1280.svg overlay.png)
 Rails.application.config.assets.precompile += %w( pic01.jpg pic02.jpg pic03.jpg)
-Rails.application.config.assets.precompile += %w( style.scss)
